136f0affb56a76fd39b8bd983cbca5c284ce678f
John Downey <johntdowney@gmail.com>
2022-03-06 16:58:13 -0700

    Cleaned up code, removed unused variables, unneeded html elements and fixed tab lengths.  No other changes.

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,58 +1,80 @@
 <template>
-    <section class="hero is-small is-primary">
-        <div class="hero-body">
-            <div>
-                <div class="block is-flex is-flex-direction-row is-flex-wrap-nowrap">
-                    <p class="title is-5 m-0 nowrap">Current Weather</p>
-                    <p class="subtitle is-6 has-text-right nowrap m-0 ml-6" v-if="!loadingUserLocation">
-                        <a class="query-input-button" @click="toggleQueryInput()">{{loadedQuery}}</a>
-                    </p>
-                </div>
-                <div v-if="editingQuery" class="block"> 
-                    <h3 class="title is-5 has-text-centered">Search by <strong>City, State</strong></h3>
-                    <form v-on:submit="submitQuery">
-                        <div class="field">
+<section class="hero is-small is-primary">
+    <div class="hero-body">
+        <div>
+            <div class="block is-flex is-flex-direction-row is-flex-wrap-nowrap">
+                <p class="title is-5 m-0 nowrap">
+                    Current Weather
+                </p>
+                <p class="subtitle is-6 has-text-right nowrap m-0 ml-6" 
+                        v-if="!loadingUserLocation">
+                    <a class="query-input-button" 
+                            @click="toggleQueryInput()">
+                        {{loadedQuery}}
+                    </a>
+                </p>
+            </div>
+            <div v-if="editingQuery" class="block"> 
+                <h3 class="title is-5 has-text-centered">
+                    Search by <strong>City, State</strong>
+                </h3>
+                <form v-on:submit="submitQuery">
+                    <div class="field">
 
-                            <input :disabled="!defaultLocation || loadingWeatherData" v-bind:class="{'is-danger':!queryValid}" class="input" v-model="query" type="text" autofocus placeholder="City, State">
+                        <input class="input" 
+                                :disabled="!defaultLocation || loadingWeatherData" 
+                                v-bind:class="{'is-danger':!queryValid}" 
+                                v-model="query" 
+                                type="text" 
+                                autofocus 
+                                placeholder="City, State">
 
-                            <p v-if="submitted && !queryValid" class="notification is-danger nowrap has-text-centered">
-                                Please use the form <strong>City, State</strong>.
-                            </p>
-
-                        </div>
-                        <div class="has-text-centered">
-                            <button :disabled="(submitted && (!queryValid || data.current.isAxiosError)) || loadingWeatherData" class="button is-success" v-bind:class="{'is-loading':loadingWeatherData}" type="submit">Submit</button>
-                        </div>
-
-                        <p class="notification is-danger nowrap has-text-centered mt-4" v-if="submitted && errorMessage">
-                            <strong>{{errorMessage}}</strong>
+                        <p class="notification is-danger nowrap has-text-centered"
+                                v-if="submitted && !queryValid"> 
+                            Please use the form <strong>City, State</strong>.
                         </p>
-                    </form>
-                </div>
-                <div class="has-text-centered m-6" v-if="loadingUserLocation">
-                    <Spinner class="spinner"></Spinner>
-                </div>
-            </div>
-            <div v-if="!editingQuery && loadedQuery">
-                
-                <div>
-                    <div class="block">
-                        <WeatherDetail v-bind:unit="unit" v-bind:weatherData="data.current" v-on:toggle-temp="toggleTempUnit"></WeatherDetail>
+
                     </div>
-                    <hr class="mb-1">
-                    <h5 class="subtitle is-6 has-text-centered m-0 mt-4 mb-1">Forecast</h5>
-                    <div class="block is-flex is-flex-direction-row is-flex-wrap-nowrap is-justify-content-space-around">
-                        <div v-for="day in data.weekly.list" v-bind:key="day.date">
-                            <WeatherForecast v-bind:unit="unit" v-bind:weatherData="day.items[0]"></WeatherForecast>
-                        </div>
+                    <div class="has-text-centered">
+                        <button class="button is-success" 
+                                :disabled="isSubmitEnabled()" 
+                                v-bind:class="{ 'is-loading' : loadingWeatherData }" 
+                                type="submit">
+                            Submit
+                        </button>
                     </div>
-                </div>
-                
-                    
+
+                    <p class="notification is-danger nowrap has-text-centered mt-4" 
+                            v-if="submitted && errorMessage">
+                        <strong>{{errorMessage}}</strong>
+                    </p>
+                </form>
             </div>
-            <h6 class="has-text-centered subtitle is-6 mt-5">Powered by <a href="https://openweathermap.org" target="_blank">openweathermap.org</a></h6>
+            <div class="has-text-centered m-6" 
+                    v-if="loadingUserLocation">
+                <Spinner class="spinner"></Spinner>
+            </div>
         </div>
-    </section>
+        <div v-if="!editingQuery && loadedQuery">
+
+            
+            <WeatherDetail v-bind:unit="unit" v-bind:weatherData="data.current" v-on:toggle-temp="toggleTempUnit"></WeatherDetail>
+            
+            <hr class="mb-1" />
+            <h5 class="subtitle is-6 has-text-centered m-0 mt-4 mb-1">Forecast</h5>
+            <div class="block is-flex is-flex-direction-row is-flex-wrap-nowrap is-justify-content-space-around">
+                <div v-for="day in data.weekly.list" 
+                        v-bind:key="day.date">
+                    <WeatherForecast v-bind:unit="unit" 
+                            v-bind:weatherData="day.items[0]"></WeatherForecast>
+                </div>
+            </div>
+        </div>
+        <h6 class="has-text-centered subtitle is-6 mt-5">
+            Powered by <a href="https://openweathermap.org" target="_blank">openweathermap.org</a>.
+        </h6>
+    </div>
+</section>
 </template>
 
 <script>
@@ -65,96 +87,91 @@ import axios from 'axios';
 import _ from 'lodash';
 
 export default {
-  name:'app',
-  components: {
-    Spinner,
-    WeatherDetail,
-    WeatherForecast
-  },
-  data() {
-    return { 
-        errorMessage:'',
-        defaultLocation: null,
-        loadingUserLocation: true,
-        loadingWeatherData: true,
-        query:'',
-        loadedQuery:'',
-        unit:'F',
-        submitted:false,
-        editingQuery: false,
-    }
-  },
-  async mounted() {
-    try {
-      // Approximate the user's location without having to ask for it.
-      let response = await axios.get('https://geolocation-db.com/json')
-      this.defaultLocation = `${response.data.city}, ${response.data.state}`;
-      this.query = this.defaultLocation;
-      this.queryCurrentWeather(response.data.city, response.data.state);
-      
-    }
-    catch(e) { console.log(e) }
-    finally { this.loadingUserLocation = false }
-  },
-  watch: {
-    query: function() {
-        this.submitted = false;
-        let match = this.query.match(/^([A-Za-z]+),[ ]?([A-Za-z]{2,})$/)
-        let s = this.query.split(',');
-        this.queryValid = s.length == 2 && s[0].trim().length > 0 && s[1].trim().length > 0;
-    }
-  },
-  methods: {
-    toggleTempUnit: function(u) {
-        this.unit = u;
+    name:'App',
+    components: {
+        Spinner,
+        WeatherDetail,
+        WeatherForecast
     },
-    submitQuery: function(e) {
-        console.log("ON SUBMIT", e);
-        e.preventDefault();
-        this.submitted = true;
-        if(this.queryValid) {
-            let s = this.query.split(',');
-            this.queryCurrentWeather(s[0].trim(), s[1].trim());
+    data() {
+        return { 
+            errorMessage:'',
+            defaultLocation: null,
+            loadingUserLocation: true,
+            loadingWeatherData: true,
+            query:'',
+            loadedQuery:'',
+            unit:'F',
+            submitted:false,
+            editingQuery: false,
         }
     },
-    toggleQueryInput: function() {
-        this.editingQuery = !this.editingQuery;
+    async mounted() {
+        try {
+            // Approximate the user's location without having to ask for it.
+            let response = await axios.get('https://geolocation-db.com/json')
+            this.defaultLocation = `${response.data.city}, ${response.data.state}`;
+            this.query = this.defaultLocation;
+            this.queryCurrentWeather(response.data.city, response.data.state);
+        }
+        catch(e) { console.err(e) }
+        finally { this.loadingUserLocation = false }
     },
-    queryCurrentWeather: function(city, state) {
-        
-        this.loadingWeatherData = true;
-        this.errorMessage = '';
-        OpenWeatherService.queryAll(city, state).then(data => {
-            
-
-             
-            if(data && !data.current.isAxiosError) {
-                this.data = data
-                this.editingQuery = data && data.current.isAxiosError;
-                this.loadedQuery = `${city}, ${state}`
-            } else if(data) {
-                this.errorMessage = data.current.response.data.message;
-            } else {
-                this.errorMessage = "Unexpected error.  Please check your internet connection and try again later."
+    watch: {
+        query: function() {
+            this.submitted = false;
+            let s = this.query.split(',');
+            this.queryValid = s.length == 2 && s[0].trim().length > 0 && s[1].trim().length > 0;
+        }
+    },
+    methods: {
+        toggleTempUnit: function(u) {
+            this.unit = u;
+        },
+        isSubmitEnabled: function() {
+            return (this.submitted && (!this.queryValid || this.data.current.isAxiosError)) || this.loadingWeatherData;
+        },
+        submitQuery: function(e) {
+            e.preventDefault();
+            this.submitted = true;
+            if(this.queryValid) {
+                let s = this.query.split(',');
+                this.queryCurrentWeather(s[0].trim(), s[1].trim());
             }
-            console.log(data);
-        })
-        .catch(err => {
-            console.log(err);
-        }).finally(()=>{
-            
-            this.loadingWeatherData = false;
-        })
-    }
-  
-  }
-  
+        },
+        toggleQueryInput: function() {
+            this.editingQuery = !this.editingQuery;
+        },
+        queryCurrentWeather: function(city, state) {
 
+            this.loadingWeatherData = true;
+            this.errorMessage = '';
+            OpenWeatherService.queryAll(city, state).then(data => {
+
+
+
+                if(data && !data.current.isAxiosError) {
+                    this.data = data
+                    this.editingQuery = data && data.current.isAxiosError;
+                    this.loadedQuery = `${city}, ${state}`
+                } else if(data) {
+                    this.errorMessage = data.current.response.data.message;
+                } else {
+                    this.errorMessage = "Unexpected error.  Please check your internet connection and try again later."
+                }
+                console.log(data);
+            }).catch(err => {
+                console.err(err);
+            }).finally(()=>{
+
+                this.loadingWeatherData = false;
+            });
+        }
+    }
 }
 </script>
 
 <style lang="sass">
-
 .spinner {
     transition: opacity 0.5s;
 }
--- src/popup/WeatherDetail.vue
+++ src/popup/WeatherDetail.vue
@@ -1,72 +1,68 @@
 <template>
+<div class="is-flex is-flex-direction-row is-flex-wrap-nowrap is-align-items-center is-justify-content-space-around">
     <div>
-        <div class="is-flex is-flex-direction-row is-flex-wrap-nowrap is-align-items-center is-justify-content-space-around">
-            <div>
-                <h4 class="subtitle is-7 has-text-centered m-0">{{displayDate(weatherData.dt)}}</h4>
-                <ul class="level">
-                    <li v-for="item in weatherData.weather" :key="item.id" class="level-item has-text-centered is-flex is-flex-direction-column">
+        <h4 class="subtitle is-7 has-text-centered m-0">
+            {{displayDate(weatherData.dt)}}
+        </h4>
+        <ul class="level">
+            <li v-for="item in weatherData.weather" 
+                    :key="item.id" 
+                    class="level-item has-text-centered is-flex is-flex-direction-column">
 
-                        <p class="heading"><img class="weather-icon" v-bind:src="'http://openweathermap.org/img/wn/'+item.icon+'@2x.png'" v-bind:title="item.description"></p>
-                        <p class="subtitle is-6 nowrap">{{item.description}}</p>
-
-
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <div class="is-flex is-flex-direction-row is-justify-content-center is-align-items-baseline">
-                    <p class="title is-1 has-text-centered m-0">{{displayTemp(weatherData.main.temp)}}</p>
-                    <p class="subtitle is-4 nowrap ml-1">
-                        <a @click="$emit('toggle-temp', 'F')" v-bind:class="{'selected-unit':unit === 'F'}">ºF</a>
-                        | <a  v-bind:class="{'selected-unit':unit === 'C'}" @click="$emit('toggle-temp', 'C')">ºC</a>
-                    </p>
-                </div>
-                <p class="subtitle is-6 has-text-centered">(feels like <strong>{{displayTemp(weatherData.main.feels_like)}}º</strong>)</p>
-            </div>
-
-        </div>
+                <p class="heading">
+                    <img class="weather-icon" 
+                            v-bind:src="`http://openweathermap.org/img/wn/${item.icon}@2x.png`" 
+                            v-bind:title="item.description"></p>
+                <p class="subtitle is-6 nowrap">
+                    {{item.description}}
+                </p>
+            </li>
+        </ul>
     </div>
+    <div>
+        <div class="is-flex is-flex-direction-row is-justify-content-center is-align-items-baseline">
+            <p class="title is-1 has-text-centered m-0">
+                {{displayTemp(weatherData.main.temp)}}
+            </p>
+            <p class="subtitle is-4 nowrap ml-1">
+                <a @click="$emit('toggle-temp', 'F')" 
+                        v-bind:class="{'selected-unit':unit === 'F'}">ºF</a>
+                |
+                <a v-bind:class="{'selected-unit':unit === 'C'}"
+                        @click="$emit('toggle-temp', 'C')">ºC</a>
+            </p>
+        </div>
+        <p class="subtitle is-6 has-text-centered">
+            (feels like <strong>{{displayTemp(weatherData.main.feels_like)}}º</strong>)
+        </p>
+    </div>
+
+</div>
 </template>
 
 <script>
-
-import Spinner from './Spinner.vue';
 import OpenWeatherService from './services/OpenWeatherService.js';
-import axios from 'axios';
-import _ from 'lodash';
-
 
 export default {
-  name:'app',
-  components: {
-    
-  },
-  data() {
-    return { 
+    name:'WeatherDetail',
+    props: ['weatherData','unit'],
+    methods: {
+        displayTemp: function(temp) {
+            return OpenWeatherService.displayTemp(temp, this.unit);
+        },
+        displayDate: function(date) {
+            return OpenWeatherService.displayDate(date);
+        }
     }
-  },
-  props:['weatherData','unit'],
-  methods: {
-  
-    displayTemp: function(temp) {
-        return OpenWeatherService.displayTemp(temp, this.unit);
-    },
-    displayDate: function(date) {
-        return OpenWeatherService.displayDate(date);
-    }
-  }
-  
-
 }
 </script>
 
 <style lang="sass">
-
 .selected-unit {
     font-weight:bold;
 }
 .weather-icon {
     margin: -15px 0 -15px 0;
-    opacity:0.7
+    opacity:0.7;
 }
 </style>
