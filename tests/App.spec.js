a164b425415eaf6be7a38268bf2ca2f224347698
John Downey <johntdowney@gmail.com>
2022-03-08 12:25:59 -0700

    Implemented two simple tests for App.vue.

---
+++ tests/App.spec.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import App from './../src/popup/App.vue'
+
+describe('App', function() {
+  
+  // Inspect the raw component options
+  it('has data', () => {
+    expect(typeof App.data).toBe('function')
+  })
+});
+
+describe('Mounted App', () => {
+  const wrapper = mount(App);
+
+  test('does a wrapper exist', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+})
