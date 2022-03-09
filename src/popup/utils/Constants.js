fb3f590f946d96a76f2c1885ee2d328b69198b42
John Downey <johntdowney@gmail.com>
2022-03-09 13:53:10 -0700

    Improved and expanded background photos.

--- src/popup/utils/Constants.js
+++ src/popup/utils/Constants.js
@@ -1,8 +1,8 @@
 export const SLIDESHOW_INTERVAL_SECONDS = 25;
 // A map of the number of photos available for each weather condition code.
 export const PHOTOS = {
-    '01d':8,'01n':7, '02d':2,'02n':3, '03d':4,'03n':3, '04d':4,'04n':4, '09d':3,
-    '09n':4, '10d':1,'10n':3, '11d':4,'11n':7, '13d':4,'13n':2, '50d':8, '50n':5 
+    '01d':11, '01n':10, '02d':3, '02n':5, '03d':7, '03n':4, '04d':3, '04n':4,  '09d':6,
+    '09n':5,  '10d':3,  '10n':4, '11d':6, '11n':9, '13d':6, '13n':4, '50d':10, '50n':7 
 }
 export const S3_BUCKET_URL = 'https://learnics-weather.s3.amazonaws.com';
 export const API_GATEWAY_URL = 'https://kwly7hekq8.execute-api.us-east-1.amazonaws.com/default/apiSecureKeys';
